49549d1508de73112b92dd6c4de492e391151311
Sagar Mundale <165837591+Sagarmundale01@users.noreply.github.com>
2024-07-28 06:51:47 +0530

    Add files via upload

---
+++ Webpage_TestNG/src/test/java/webpage/ui/GroupsDemoTest2.java
@@ -0,0 +1,44 @@
+package webpage.ui;
+
+import org.testng.annotations.Test;
+
+import common.CommonDataSetUp;
+
+
+public class GroupsDemoTest2 extends CommonDataSetUp{
+	
+	@Test (priority = 1, groups = "Regression")
+	public void aTest1()
+	{
+		System.out.println("Test1");
+	} 
+	
+	
+	@Test(priority = 2, groups ={"Regression", "bvt"} )
+	public void bTest2() 
+	{
+	  System.out.println("Test2");
+			
+	}
+	
+	
+	@Test(priority = 3, groups = {"Regression","bvt"})
+	public void cTest3() 
+	{
+	  System.out.println("Test3");
+			
+	}
+	
+	@Test(priority = 4, groups = "bvt")
+	public void dTest4() 
+	{
+	  System.out.println("Test4");
+			
+	}
+	
+	
+	
+	
+	
+
+}
---
+++ Webpage_TestNG/src/test/java/webpage/ui/Suite_Annotation.java
@@ -0,0 +1,69 @@
+package webpage.ui;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterGroups;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeGroups;
+import org.testng.annotations.Test;
+
+public class Suite_Annotation {
+
+	@BeforeClass
+	public void beforeClass()
+	{
+		System.out.println("Run this before class");
+	} 
+	
+	
+	@AfterClass
+	public void afterClass() 
+	{
+	  System.out.println("Run this after class");
+			
+	}
+	
+	@BeforeGroups(value = "regression")
+	public void beforeGroup()
+	{
+		System.out.println("Run this method before regression");
+	} 
+	
+	@AfterGroups(value = "regression")
+	public void afterGroups()
+	{
+		System.out.println("Run this method after regression");
+	} 
+	
+	
+	@Test (priority = 1, groups = "regression")
+	public void aTest1()
+	{
+		System.out.println("Test1");
+	} 
+	
+	
+	@Test(priority = 2, groups ={"regression", "bvt"} )
+	public void bTest2() 
+	{
+	  System.out.println("Test2");
+			
+	}
+	
+	
+	@Test(priority = 3, groups = {"regression","bvt"})
+	public void cTest3() 
+	{
+	  System.out.println("Test3");
+			
+	}
+	
+	@Test(priority = 4, groups = "bvt")
+	public void dTest4() 
+	{
+	  System.out.println("Test4");
+			
+	}
+	
+	
+	
+}
